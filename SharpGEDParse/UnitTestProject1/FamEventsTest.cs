1096cf541bfd722be4c33b1e40017928a8cdb634
fire-eggs <lifeattickville@gmail.com>
2015-08-17 15:51:29 -0400

    Exercise AGE tags for either spouse

--- SharpGEDParse/UnitTestProject1/FamEventsTest.cs
+++ SharpGEDParse/UnitTestProject1/FamEventsTest.cs
@@ -15,8 +15,33 @@ namespace UnitTestProject1
         // TODO object
         // TODO CHAN
 
-        // TODO HUSB + age
-        // TODO WIFE + age
+        /*
+1 MARR
+2 DATE 31 DEC 1997
+2 PLAC The place
+2 TYPE Man and Wife
+2 ADDR A Church3 CONT Main Street, USA
+2 CAUS Love
+2 AGNC Catholic Church
+2 HUSB
+3 AGE 42y
+2 WIFE
+3 AGE 42y 6m
+2 OBJE
+3 FORM jpeg
+3 TITL Multimedia link about the marriage event
+3 FILE ImgFile.JPG
+2 SOUR @SOURCE1@
+3 PAGE 42
+3 DATA
+4 DATE 31 DEC 1900
+4 TEXT Text from marriage source.
+3 QUAY 3
+3 NOTE A note about the marriage source.
+2 NOTE Marriage event note (a legal, common-law, or customary event of creating a family 
+3 CONC unit of a man and a woman as husband and wife).        
+         */
+
 
         public KBRGedFam TestEventTag(string tag)
         {
@@ -37,7 +62,7 @@ namespace UnitTestProject1
             var rec = parse(indi3);
             Assert.AreEqual(1, rec.FamEvents.Count);
             Assert.AreEqual(tag, rec.FamEvents[0].Tag);
-            Assert.AreEqual("Y", rec.FamEvents[0].Detail); // TODO 'MARR' specific?
+            Assert.AreEqual("Y", rec.FamEvents[0].Detail); // TODO 'MARR' specific? NOTE also occurs with ENGA in "ged complete"
             Assert.AreEqual("1774", rec.FamEvents[0].Date);
             Assert.AreEqual("suspicious", rec.FamEvents[0].Type);
             Assert.AreEqual(null, rec.FamEvents[0].Place);
@@ -48,12 +73,38 @@ namespace UnitTestProject1
             return rec;
         }
 
+        public KBRGedFam TestEventAge(string tag, string spouse)
+        {
+            string fam = string.Format("0 FAM\n1 {0}\n2 {1}\n3 AGE 42\n2 PLAC Sands, Oldham, Lncshr, Eng\n2 AGE 99", tag, spouse);
+            var rec = parse(fam);
+            Assert.AreEqual(1, rec.FamEvents.Count);
+            KBRGedEvent famEvent = rec.FamEvents[0];
+            Assert.AreEqual(tag, famEvent.Tag);
+            Assert.AreEqual(null, famEvent.Date);
+            Assert.AreEqual("99", famEvent.Age);
+            Assert.AreEqual(null, famEvent.Type);
+            Assert.AreEqual("Sands, Oldham, Lncshr, Eng", famEvent.Place);
+            if (spouse == "HUSB")
+            {
+                Assert.AreEqual("42", famEvent.HusbAge);
+                Assert.AreEqual(null, famEvent.WifeAge);
+            }
+            else
+            {
+                Assert.AreEqual(null, famEvent.HusbAge);
+                Assert.AreEqual("42", famEvent.WifeAge);
+            }
+            return rec;
+        }
+
         [TestMethod]
         public void TestMarr()
         {
             string tag = "MARR";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestAnul()
@@ -61,6 +112,8 @@ namespace UnitTestProject1
             string tag = "ANUL";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestMarl()
@@ -68,6 +121,8 @@ namespace UnitTestProject1
             string tag = "MARL";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestMars()
@@ -75,6 +130,8 @@ namespace UnitTestProject1
             string tag = "MARS";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestMarb()
@@ -82,6 +139,8 @@ namespace UnitTestProject1
             string tag = "MARB";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestMarc()
@@ -89,6 +148,8 @@ namespace UnitTestProject1
             string tag = "MARC";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestDiv()
@@ -96,6 +157,8 @@ namespace UnitTestProject1
             string tag = "DIV";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestDivf()
@@ -103,6 +166,8 @@ namespace UnitTestProject1
             string tag = "DIVF";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestEnga()
@@ -110,6 +175,8 @@ namespace UnitTestProject1
             string tag = "ENGA";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
         [TestMethod]
         public void TestEven()
@@ -117,6 +184,8 @@ namespace UnitTestProject1
             string tag = "EVEN";
             TestEventTag(tag);
             TestEventTag2(tag);
+            TestEventAge(tag, "HUSB");
+            TestEventAge(tag, "WIFE");
         }
 
         [TestMethod]
