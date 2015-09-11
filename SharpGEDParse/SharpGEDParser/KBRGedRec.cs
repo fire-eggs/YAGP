d55c76cb6816956ce4d82490a28aec47e039b2a7
fire-eggs <lifeattickville@gmail.com>
2015-09-11 11:50:46 -0400

    cleanup

--- SharpGEDParse/SharpGEDParser/KBRGedRec.cs
+++ SharpGEDParse/SharpGEDParser/KBRGedRec.cs
@@ -25,18 +25,6 @@ namespace SharpGEDParser
             return string.Format("KBRGedRec:{0}:{1}:{2}", Tag, Ident, Lines);
         }
 
-        public virtual void Parse()
-        {
-            // TODO make abstract?
-            // TODO parse sub-record data: OVERRIDE ME
-        }
-
-        public virtual void Validate()
-        {
-            // TODO make abstract?
-            // TODO check lines and add errors to an error set OVERRIDE ME
-        }
-
         public string Ident { get; set; }
         public string Tag { get; set; }
         public List<UnkRec> Unknowns { get; set; }
