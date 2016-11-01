6bc85e4184b066a884e8d31cb9ec38d7e37639e2
fire-eggs <lifeattickville@gmail.com>
2016-11-01 15:23:02 -0400

    Basic name parsing to fix some unit tests

--- SharpGEDParse/SharpGEDParser/Parser/IndiParse.cs
+++ SharpGEDParse/SharpGEDParser/Parser/IndiParse.cs
@@ -128,14 +128,38 @@ namespace SharpGEDParser.Parser
             }
         }
 
-        private void NameProc(ParseContext2 context)
+        private void NameProc(ParseContext2 ctx)
         {
             var rec = new NameRec();
             rec.Beg = ctx.Begline;
             rec.End = ctx.Endline;
-            rec.Names = context.Remain.Trim();
-            (context.Parent as IndiRecord).Names.Add(rec);
+
+            string line = ctx.Remain;
+            int max = line.Length;
+
+            // BOULDER_CEM_02212009b.GED had a "1 NAME" with nothing else
+            int startName = GedLineUtil.FirstChar(line, 0, max);
+            if (startName >= 0)
+            {
+                int startSur = GedLineUtil.AllCharsUntil(line, max, startName, '/');
+                int endSur = GedLineUtil.AllCharsUntil(line, max, startSur + 1, '/');
+
+                var suffix = "";
+                if (endSur + 1 < max)
+                    suffix = line.Substring(endSur + 1).Trim();
+
+                rec.Names = line.Substring(startName, startSur - startName).Trim();
+                rec.Names = string.Join(" ", rec.Names.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)); // Remove extra spaces
+                if (startSur < max) // e.g. "1 NAME LIVING"
+                    rec.Surname = line.Substring(startSur + 1, endSur - startSur - 1);
+                if (suffix.Length > 0)
+                    rec.Suffix = suffix;
+            }
+
+            //rec.Names = context.Remain.Trim();
+            (ctx.Parent as IndiRecord).Names.Add(rec);
             // TODO parsing details
+            // TODO parsing sub-records
         }
 
         private void EventProc(ParseContext2 context)
---
+++ SharpGEDParse/SharpGEDParser/Tests/IndiNames.cs
@@ -0,0 +1,87 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using SharpGEDParser.Model;
+
+// ReSharper disable ConvertToConstant.Local
+// ReSharper disable InconsistentNaming
+
+// TODO a surname with space? e.g. "von neumann"? extra spaces?
+// TODO name sub-records
+
+namespace SharpGEDParser.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    class IndiNames : GedParseTest
+    {
+        private IndiRecord parse(string val)
+        {
+            return parse<IndiRecord>(val);
+        }
+
+        [Test]
+        public void TestBasicName()
+        {
+            var indi1 = "0 INDI\n1 NAME  kludge  ";
+            var rec = parse(indi1);
+            Assert.AreEqual(1, rec.Names.Count);
+            Assert.AreEqual("kludge", rec.Names[0].Names);
+            Assert.AreEqual(null, rec.Names[0].Surname);
+        }
+
+        [Test]
+        public void TestBasicSurname()
+        {
+            var indi1 = "0 INDI\n1 NAME kludge /clan/";
+            var indi2 = "0 INDI\n1 NAME /clan2/";
+            var rec = parse(indi1);
+            Assert.AreEqual(1, rec.Names.Count);
+            Assert.AreEqual("kludge", rec.Names[0].Names);
+            Assert.AreEqual("clan", rec.Names[0].Surname);
+            rec = parse(indi2);
+            Assert.AreEqual(1, rec.Names.Count);
+            Assert.AreEqual("", rec.Names[0].Names);
+            Assert.AreEqual("clan2", rec.Names[0].Surname);
+        }
+
+        [Test]
+        public void TestBasicSurname2()
+        {
+            // real person from a GED, extra spaces
+            var indi1 = "0 INDI\n1    NAME     Marjorie    Lee     /Smith/   \n2 GIVN Marjorie Lee\n2 SURN Smith";
+
+            var rec = parse(indi1);
+            Assert.AreEqual(1, rec.Names.Count);
+            Assert.AreEqual("Marjorie Lee", rec.Names[0].Names, "Failure to remove extra spaces"); // TODO failure to strip extra spaces
+            Assert.AreEqual("Smith", rec.Names[0].Surname);
+        }
+
+        [Test]
+        public void TestMultiName()
+        {
+            var indi = "0 INDI\n1 NAME John /Smith/\n1 NAME Eric /Jones/";
+            var indi2 = "0 INDI\n1 NAME John /Smith/\n1 SEX M\n2 NOTE blah blah\n1 NAME Eric /Jones/";
+            var rec = parse(indi);
+            Assert.AreEqual(2, rec.Names.Count);
+            Assert.AreEqual("Smith", rec.Names[0].Surname);
+            Assert.AreEqual("Jones", rec.Names[1].Surname);
+            rec = parse(indi2);
+            Assert.AreEqual(2, rec.Names.Count);
+            Assert.AreEqual("Smith", rec.Names[0].Surname);
+            Assert.AreEqual("Jones", rec.Names[1].Surname);
+        }
+
+        [Test]
+        public void TestSuffix()
+        {
+            var indi = "0 INDI\n1 NAME Given Name /Smith/ jr ";
+            var indi2 = "0 INDI\n1 NAME Given Name /Smith/esq";
+            var rec = parse(indi);
+            Assert.AreEqual(1, rec.Names.Count);
+            Assert.AreEqual("jr", rec.Names[0].Suffix);
+            rec = parse(indi2);
+            Assert.AreEqual(1, rec.Names.Count);
+            Assert.AreEqual("esq", rec.Names[0].Suffix);
+        }
+    }
+}
