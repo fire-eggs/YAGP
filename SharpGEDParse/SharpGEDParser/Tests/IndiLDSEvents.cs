8005adf41d831acd9cb24a200826b56526b6be3c
fire-eggs <lifeattickville@gmail.com>
2016-11-23 19:53:39 -0500

    Add errors list to sub-structure: NOT SURE THIS IS BEST

--- SharpGEDParse/SharpGEDParser/Tests/IndiLDSEvents.cs
+++ SharpGEDParse/SharpGEDParser/Tests/IndiLDSEvents.cs
@@ -76,7 +76,26 @@ namespace SharpGEDParser.Tests
             Assert.AreEqual("salt lake", rec.LDSEvents[0].Temple);
             Assert.AreEqual("salty", rec.LDSEvents[0].Place);
             Assert.AreEqual("insane", rec.LDSEvents[0].Status);
+            Assert.AreEqual(0, rec.LDSEvents[0].Errors.Count);
             Assert.AreEqual("foo", rec.LDSEvents[0].FamilyXref);
+        }
+
+        [Test]
+        public void SlgcErrXref()
+        {
+            // error in FamilyXref
+            var indi = "0 INDI\n1 SLGC\n2 DATE unk\n2 TEMP salt lake\n2 NOTE note1\n2 PLAC salty\n2 STAT insane\n3 DATE statdate\n2 NOTE note2\n2 SOUR @s1@\n2 FAMC @ @";
+            var rec = parse(indi);
+
+            Assert.AreEqual(1, rec.LDSEvents.Count);
+            Assert.AreEqual("SLGC", rec.LDSEvents[0].Tag);
+            Assert.AreEqual("statdate", rec.LDSEvents[0].Date); // TODO real date parsing
+            Assert.AreEqual("salt lake", rec.LDSEvents[0].Temple);
+            Assert.AreEqual("salty", rec.LDSEvents[0].Place);
+            Assert.AreEqual("insane", rec.LDSEvents[0].Status);
+            Assert.AreEqual(1, rec.LDSEvents[0].Errors.Count);
+            Assert.IsNotNullOrEmpty(rec.LDSEvents[0].Errors[0].Error);
+            Assert.IsNullOrEmpty(rec.LDSEvents[0].FamilyXref);
         }
 
         [Test]
