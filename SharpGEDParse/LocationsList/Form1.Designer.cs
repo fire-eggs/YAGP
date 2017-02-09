32646211dafc6be6058287e9d714db05ffd4b9e8
fire-eggs <lifeattickville@gmail.com>
2017-02-09 12:34:20 -0500

    start of the locations lister utility

---
+++ SharpGEDParse/LocationsList/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace LocationsList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadGED = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AllEvents = new System.Windows.Forms.RadioButton();
+            this.SelectedEvents = new System.Windows.Forms.RadioButton();
+            this.EventsPick = new System.Windows.Forms.TreeView();
+            this.LocOnly = new System.Windows.Forms.RadioButton();
+            this.LocSurname = new System.Windows.Forms.RadioButton();
+            this.LocPeople = new System.Windows.Forms.RadioButton();
+            this.eventsHolder = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.OutFileSelect = new System.Windows.Forms.Button();
+            this.OutFileDisplay = new System.Windows.Forms.Label();
+            this.DoIt = new System.Windows.Forms.Button();
+            this.CloseIt = new System.Windows.Forms.Button();
+            this.eventsHolder.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LoadGED
+            // 
+            this.LoadGED.Location = new System.Drawing.Point(13, 13);
+            this.LoadGED.Name = "LoadGED";
+            this.LoadGED.Size = new System.Drawing.Size(109, 23);
+            this.LoadGED.TabIndex = 0;
+            this.LoadGED.Text = "Load GEDCOM";
+            this.LoadGED.UseVisualStyleBackColor = true;
+            this.LoadGED.Click += new System.EventHandler(this.LoadGED_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // AllEvents
+            // 
+            this.AllEvents.AutoSize = true;
+            this.AllEvents.Checked = true;
+            this.AllEvents.Location = new System.Drawing.Point(3, 3);
+            this.AllEvents.Name = "AllEvents";
+            this.AllEvents.Size = new System.Drawing.Size(72, 17);
+            this.AllEvents.TabIndex = 4;
+            this.AllEvents.TabStop = true;
+            this.AllEvents.Text = "All Events";
+            this.AllEvents.UseVisualStyleBackColor = true;
+            // 
+            // SelectedEvents
+            // 
+            this.SelectedEvents.AutoSize = true;
+            this.SelectedEvents.Location = new System.Drawing.Point(3, 27);
+            this.SelectedEvents.Name = "SelectedEvents";
+            this.SelectedEvents.Size = new System.Drawing.Size(106, 17);
+            this.SelectedEvents.TabIndex = 5;
+            this.SelectedEvents.TabStop = true;
+            this.SelectedEvents.Text = "Selected Events:";
+            this.SelectedEvents.UseVisualStyleBackColor = true;
+            // 
+            // EventsPick
+            // 
+            this.EventsPick.CheckBoxes = true;
+            this.EventsPick.Location = new System.Drawing.Point(3, 51);
+            this.EventsPick.Name = "EventsPick";
+            this.EventsPick.Size = new System.Drawing.Size(204, 211);
+            this.EventsPick.TabIndex = 6;
+            this.EventsPick.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.EventsPick_AfterCheck);
+            // 
+            // LocOnly
+            // 
+            this.LocOnly.AutoSize = true;
+            this.LocOnly.Checked = true;
+            this.LocOnly.Location = new System.Drawing.Point(3, 3);
+            this.LocOnly.Name = "LocOnly";
+            this.LocOnly.Size = new System.Drawing.Size(95, 17);
+            this.LocOnly.TabIndex = 7;
+            this.LocOnly.TabStop = true;
+            this.LocOnly.Text = "Locations Only";
+            this.LocOnly.UseVisualStyleBackColor = true;
+            // 
+            // LocSurname
+            // 
+            this.LocSurname.AutoSize = true;
+            this.LocSurname.Location = new System.Drawing.Point(3, 37);
+            this.LocSurname.Name = "LocSurname";
+            this.LocSurname.Size = new System.Drawing.Size(142, 17);
+            this.LocSurname.TabIndex = 8;
+            this.LocSurname.TabStop = true;
+            this.LocSurname.Text = "Locations and Surnames";
+            this.LocSurname.UseVisualStyleBackColor = true;
+            // 
+            // LocPeople
+            // 
+            this.LocPeople.AutoSize = true;
+            this.LocPeople.Location = new System.Drawing.Point(3, 72);
+            this.LocPeople.Name = "LocPeople";
+            this.LocPeople.Size = new System.Drawing.Size(128, 17);
+            this.LocPeople.TabIndex = 9;
+            this.LocPeople.TabStop = true;
+            this.LocPeople.Text = "Locations and People";
+            this.LocPeople.UseVisualStyleBackColor = true;
+            // 
+            // eventsHolder
+            // 
+            this.eventsHolder.Controls.Add(this.AllEvents);
+            this.eventsHolder.Controls.Add(this.SelectedEvents);
+            this.eventsHolder.Controls.Add(this.EventsPick);
+            this.eventsHolder.Location = new System.Drawing.Point(13, 59);
+            this.eventsHolder.Name = "eventsHolder";
+            this.eventsHolder.Size = new System.Drawing.Size(215, 268);
+            this.eventsHolder.TabIndex = 10;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LocOnly);
+            this.panel1.Controls.Add(this.LocSurname);
+            this.panel1.Controls.Add(this.LocPeople);
+            this.panel1.Location = new System.Drawing.Point(247, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 185);
+            this.panel1.TabIndex = 11;
+            // 
+            // OutFileSelect
+            // 
+            this.OutFileSelect.Location = new System.Drawing.Point(13, 334);
+            this.OutFileSelect.Name = "OutFileSelect";
+            this.OutFileSelect.Size = new System.Drawing.Size(109, 23);
+            this.OutFileSelect.TabIndex = 12;
+            this.OutFileSelect.Text = "Write to...";
+            this.OutFileSelect.UseVisualStyleBackColor = true;
+            this.OutFileSelect.Click += new System.EventHandler(this.OutFileSelect_Click);
+            // 
+            // OutFileDisplay
+            // 
+            this.OutFileDisplay.AutoSize = true;
+            this.OutFileDisplay.Location = new System.Drawing.Point(137, 339);
+            this.OutFileDisplay.Name = "OutFileDisplay";
+            this.OutFileDisplay.Size = new System.Drawing.Size(0, 13);
+            this.OutFileDisplay.TabIndex = 13;
+            // 
+            // DoIt
+            // 
+            this.DoIt.Location = new System.Drawing.Point(164, 376);
+            this.DoIt.Name = "DoIt";
+            this.DoIt.Size = new System.Drawing.Size(75, 23);
+            this.DoIt.TabIndex = 14;
+            this.DoIt.Text = "Do It";
+            this.DoIt.UseVisualStyleBackColor = true;
+            this.DoIt.Click += new System.EventHandler(this.DoIt_Click);
+            // 
+            // CloseIt
+            // 
+            this.CloseIt.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.CloseIt.Location = new System.Drawing.Point(427, 376);
+            this.CloseIt.Name = "CloseIt";
+            this.CloseIt.Size = new System.Drawing.Size(75, 23);
+            this.CloseIt.TabIndex = 15;
+            this.CloseIt.Text = "Close";
+            this.CloseIt.UseVisualStyleBackColor = true;
+            this.CloseIt.Click += new System.EventHandler(this.CloseIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 411);
+            this.Controls.Add(this.CloseIt);
+            this.Controls.Add(this.DoIt);
+            this.Controls.Add(this.OutFileDisplay);
+            this.Controls.Add(this.OutFileSelect);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.eventsHolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoadGED);
+            this.Name = "Form1";
+            this.Text = "GEDCOM Location Lister";
+            this.eventsHolder.ResumeLayout(false);
+            this.eventsHolder.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadGED;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton AllEvents;
+        private System.Windows.Forms.RadioButton SelectedEvents;
+        private System.Windows.Forms.TreeView EventsPick;
+        private System.Windows.Forms.RadioButton LocOnly;
+        private System.Windows.Forms.RadioButton LocSurname;
+        private System.Windows.Forms.RadioButton LocPeople;
+        private System.Windows.Forms.Panel eventsHolder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button OutFileSelect;
+        private System.Windows.Forms.Label OutFileDisplay;
+        private System.Windows.Forms.Button DoIt;
+        private System.Windows.Forms.Button CloseIt;
+    }
+}
+
---
+++ SharpGEDParse/LocationsList/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using GEDWrap;
+using SharpGEDParser.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LocationsList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DoItState();
+        }
+
+        private void LoadGED_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "GEDCOM files|*.ged;*.GED";
+            ofd.FilterIndex = 1;
+            ofd.DefaultExt = "ged";
+            ofd.CheckFileExists = true;
+            if (DialogResult.OK != ofd.ShowDialog(this))
+            {
+                return;
+            }
+            label1.Text = ofd.FileName;
+
+            LoadIt(ofd.FileName); // TODO background load
+        }
+
+        private void LoadIt(string path)
+        {
+            // Load a GED file.
+            using (Forest f = new Forest())
+            {
+                f.LoadGEDCOM(path);
+                ScanIt(f);
+            }
+            // Scan for events w/ locations
+            // Update events tree
+            BuildTagTree();
+            // Enable controls
+            DoItState();
+        }
+
+        private class treeHier
+        {
+            public int Group;
+            public string Tag;
+            public string Label;
+        }
+
+        private List<treeHier> displayHier = new List<treeHier>
+        {
+            new treeHier {Group=1, Tag=null, Label = "INDI Events"},
+            new treeHier {Group=1, Tag="BIRT", Label = "Birth"},
+            new treeHier {Group=1, Tag="CHR", Label = "Christening"},
+            new treeHier {Group=1, Tag="DEAT", Label = "Death"},
+            new treeHier {Group=1, Tag="BURI", Label = ""},
+            new treeHier {Group=1, Tag="CREM", Label = ""},
+            new treeHier {Group=1, Tag="ADOP", Label = ""},
+            new treeHier {Group=1, Tag="BAPM", Label = ""},
+            new treeHier {Group=1, Tag="BARM", Label = ""},
+            new treeHier {Group=1, Tag="BASM", Label = ""},
+            new treeHier {Group=1, Tag="BLES", Label = ""},
+            new treeHier {Group=1, Tag="CHRA", Label = ""},
+            new treeHier {Group=1, Tag="CONF", Label = ""},
+            new treeHier {Group=1, Tag="FCOM", Label = ""},
+            new treeHier {Group=1, Tag="ORDN", Label = ""},
+            new treeHier {Group=1, Tag="NATU", Label = ""},
+            new treeHier {Group=1, Tag="EMIG", Label = ""},
+            new treeHier {Group=1, Tag="IMMI", Label = ""},
+            new treeHier {Group=1, Tag="CENS", Label = ""},
+            new treeHier {Group=1, Tag="PROB", Label = ""},
+            new treeHier {Group=1, Tag="WILL", Label = ""},
+            new treeHier {Group=1, Tag="GRAD", Label = ""},
+            new treeHier {Group=1, Tag="RETI", Label = ""},
+            new treeHier {Group=1, Tag="EVEN", Label = ""}, // conflict
+            new treeHier {Group=2, Tag=null, Label = "INDI Attributes"},
+            new treeHier {Group=2, Tag="CAST", Label = "Caste"},
+            new treeHier {Group=2, Tag="DSCR", Label = "Description"},
+            new treeHier {Group=2, Tag="EDUC", Label = ""},
+            new treeHier {Group=2, Tag="IDNO", Label = ""},
+            new treeHier {Group=2, Tag="NATI", Label = ""},
+            new treeHier {Group=2, Tag="NCHI", Label = ""},
+            new treeHier {Group=2, Tag="NMR", Label = ""},
+            new treeHier {Group=2, Tag="OCCU", Label = ""},
+            new treeHier {Group=2, Tag="PROP", Label = ""},
+            new treeHier {Group=2, Tag="RELI", Label = ""},
+            new treeHier {Group=2, Tag="RESI", Label = ""}, // conflict
+            new treeHier {Group=2, Tag="SSN", Label = ""},
+            new treeHier {Group=2, Tag="TITL", Label = ""},
+            new treeHier {Group=2, Tag="FACT", Label = ""},
+            new treeHier {Group=3, Tag=null, Label = "FAM Events"},
+            new treeHier {Group=3, Tag="MARR", Label = ""},
+            new treeHier {Group=3, Tag="ANUL", Label = ""},
+            new treeHier {Group=3, Tag="CENS", Label = ""},
+            new treeHier {Group=3, Tag="DIV", Label = ""},
+            new treeHier {Group=3, Tag="DIVF", Label = ""},
+            new treeHier {Group=3, Tag="ENGA", Label = ""},
+            new treeHier {Group=3, Tag="MARB", Label = ""},
+            new treeHier {Group=3, Tag="MARC", Label = ""},
+            new treeHier {Group=3, Tag="MARL", Label = ""},
+            new treeHier {Group=3, Tag="MARS", Label = ""},
+            new treeHier {Group=3, Tag="RESI", Label = ""}, // conflict
+            new treeHier {Group=3, Tag="EVEN", Label = ""}, // conflict
+        };
+
+        private class One
+        {
+            public string Location;
+            public string Tag;
+            public string PersonId;
+            public string FamId;
+        }
+
+        private List<One> dataSet;
+
+        private void ScanIt(Forest f)
+        {
+            dataSet = new List<One>();
+
+            foreach (var person in f.AllPeople)
+            {
+                IndiRecord ged = person.Indi;
+                foreach (var familyEvent in ged.Events)
+                {
+                    string tag = familyEvent.Tag;
+                    if (!string.IsNullOrEmpty(familyEvent.Place))
+                    {
+                        dataSet.Add(new One {Location = familyEvent.Place, Tag = tag, PersonId = ged.Ident});
+                    }
+                }
+                foreach (var familyEvent in ged.Attribs)
+                {
+                    string tag = familyEvent.Tag;
+                    if (!string.IsNullOrEmpty(familyEvent.Place))
+                    {
+                        dataSet.Add(new One { Location = familyEvent.Place, Tag = tag, PersonId = ged.Ident });
+                    }
+                }
+            }
+
+            foreach (var union in f.AllUnions)
+            {
+                FamRecord fam = union.FamRec;
+                foreach (var familyEvent in fam.FamEvents)
+                {
+                    string tag = familyEvent.Tag;
+                    if (!string.IsNullOrEmpty(familyEvent.Place))
+                    {
+                        dataSet.Add(new One { Location = familyEvent.Place, Tag = tag, FamId = fam.Ident });
+                    }
+                }
+            }
+        }
+
+        private void BuildTagTree()
+        {
+            var uniqueTags = (from one in dataSet select one.Tag).Distinct(); //.OrderBy(name => name);
+            var roots = (from hier in displayHier where hier.Tag == null select hier);
+            var ents = (from hier in displayHier where uniqueTags.Contains(hier.Tag) select hier).ToArray();
+
+            List<TreeNode> nodes = new List<TreeNode>();
+            foreach (var treeHier in roots)
+            {
+                treeHier hier1 = treeHier;
+                TreeNode node = new TreeNode(hier1.Label) { Tag = hier1, Checked = true };
+                var childs = (from hier in ents where hier.Tag != null && hier.Group == hier1.Group select hier);
+                foreach (var child in childs)
+                {
+                    TreeNode cNode = new TreeNode(child.Label == "" ? child.Tag : child.Label) {Tag = child, Checked = true};
+                    node.Nodes.Add(cNode);
+                }
+                nodes.Add(node);
+            }
+
+            EventsPick.SuspendLayout();
+            EventsPick.Nodes.Clear();
+            EventsPick.Nodes.AddRange(nodes.ToArray());
+            EventsPick.ResumeLayout();
+        }
+
+        private List<string> GetCheckedEvents()
+        {
+            List<string> checkedEvents = new List<string>();
+            foreach (TreeNode node in EventsPick.Nodes)
+            {
+                foreach (TreeNode child in node.Nodes)
+                {
+                    if (!child.Checked)
+                        continue;
+                    treeHier data = child.Tag as treeHier;
+                    if (data != null && !string.IsNullOrEmpty(data.Tag))
+                        checkedEvents.Add(data.Tag);
+                }
+            }
+            return checkedEvents;
+        }
+
+        #region Tree Checkbox State
+        private bool busy = false; // prevent recursive check events
+
+        // TODO uncheck of child changes parent state
+
+        private void EventsPick_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (busy) return;
+            busy = true;
+            try
+            {
+                checkNodes(e.Node, e.Node.Checked);
+            }
+            finally
+            {
+                busy = false;
+            }
+        }
+
+        private void checkNodes(TreeNode node, bool check)
+        {
+            foreach (TreeNode node1 in node.Nodes)
+            {
+                node1.Checked = check;
+                checkNodes(node1, check);
+            }
+        }
+        #endregion
+
+        private void OutFileSelect_Click(object sender, EventArgs e)
+        {
+            var ofd = new SaveFileDialog();
+            ofd.Filter = "Text files|*.txt;*.TXT";
+            ofd.FilterIndex = 1;
+            ofd.DefaultExt = "txt";
+            if (DialogResult.OK != ofd.ShowDialog(this))
+            {
+                return;
+            }
+            OutFileDisplay.Text = ofd.FileName;
+            DoItState();
+        }
+
+        private void DoItState()
+        {
+            DoIt.Enabled = !string.IsNullOrWhiteSpace(OutFileDisplay.Text);
+        }
+
+        private void DoIt_Click(object sender, EventArgs e)
+        {
+            string outfile = OutFileDisplay.Text;
+            if (string.IsNullOrWhiteSpace(outfile))
+                return;
+
+            List<One> filteredData = dataSet;
+            if (!AllEvents.Checked)
+            {
+                List<string> checkedEvents = GetCheckedEvents();
+                if (checkedEvents.Count != 0)
+                {
+                    filteredData = (from one in dataSet where checkedEvents.Contains(one.Tag) select one).ToList();
+                }
+            }
+
+            if (LocOnly.Checked)
+            {
+                WriteLocationsOnly(filteredData, outfile);
+            }
+
+            Process.Start("notepad.exe", outfile);
+        }
+
+        private void CloseIt_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void WriteLocationsOnly(List<One> filteredData, string path)
+        {
+            var sb = new StringBuilder();
+            //foreach (var one in filteredData)
+            //{
+            //    sb.AppendFormat("{0}:{1}", one.Tag, one.Location);
+            //    sb.AppendLine();
+            //}
+            var uniqueLocs = (from one in filteredData select one.Location).Distinct().OrderBy(name => name);
+            foreach (var loc in uniqueLocs)
+            {
+                sb.AppendLine(loc);
+            }
+            File.WriteAllText(path, sb.ToString());
+        }
+    }
+}
