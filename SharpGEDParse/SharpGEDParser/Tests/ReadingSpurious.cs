1cc250cb05733ac83de69da09e3a1b448a43589f
fire-eggs <lifeattickville@gmail.com>
2017-08-09 16:21:30 -0400

    One last reading-physical-file test

---
+++ SharpGEDParse/SharpGEDParser/Tests/ReadingSpurious.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+using SharpGEDParser.Model;
+
+// ReSharper disable InconsistentNaming
+
+// Exercise various spurious line break scenarios
+namespace SharpGEDParser.Tests
+{
+    [TestFixture]
+    class ReadingSpurious : ReadingUtil
+    {
+        private readonly string[] linesLF =
+        {
+            "0 HEAD",
+            "1 CHAR ASCII",
+            "1 SO\nUR 0",
+            "0 TRLR",
+        };
+
+        private readonly string[] linesCR =
+        {
+            "0 HEAD",
+            "1 CHAR ASCII",
+            "1 SO\rUR 0",
+            "0 TRLR",
+        };
+
+        [Test]
+        public void LFinDOS()
+        {
+            var r = BuildAndRead(linesLF, GedReader.LB.DOS, false, true);
+            Assert.AreEqual(linesLF.Length, r.NumberLines-1);
+            Assert.AreEqual(0, r.Errors.Count);
+
+            Assert.AreEqual(1, r.Data.Count);
+            var rec = r.Data[0] as HeadRecord;
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("0", rec.Source);
+        }
+
+        [Test]
+        public void CRinUnix()
+        {
+            var r = BuildAndRead(linesCR, GedReader.LB.UNIX, false, true);
+            Assert.AreEqual(0, r.Errors.Count);
+            Assert.AreEqual(linesCR.Length, r.NumberLines - 1);
+
+            Assert.AreEqual(1, r.Data.Count);
+            var rec = r.Data[0] as HeadRecord;
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("0", rec.Source);
+        }
+
+        [Test]
+        public void LFinDOSBom()
+        {
+            var r = BuildAndRead(linesLF, GedReader.LB.DOS, true, true);
+            Assert.AreEqual(linesLF.Length, r.NumberLines - 1);
+
+            // Error: BOM / Head.Char mismatch
+            Assert.AreEqual(1, r.Errors.Count); // TODO eliminate?
+
+            Assert.AreEqual(1, r.Data.Count);
+            var rec = r.Data[0] as HeadRecord;
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("0", rec.Source);
+        }
+
+        [Test]
+        public void CRinUnixBom()
+        {
+            var r = BuildAndRead(linesCR, GedReader.LB.UNIX, true, true);
+            Assert.AreEqual(linesCR.Length, r.NumberLines - 1);
+
+            // Error: BOM / Head.Char mismatch
+            Assert.AreEqual(1, r.Errors.Count); // TODO eliminate?
+
+            Assert.AreEqual(1, r.Data.Count);
+            var rec = r.Data[0] as HeadRecord;
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("0", rec.Source);
+        }
+
+        [Test]
+        public void LFinDOSNoEnd()
+        {
+            // No final line terminator
+            var r = BuildAndRead(linesLF, GedReader.LB.DOS, false, false);
+            Assert.AreEqual(linesLF.Length, r.NumberLines - 1);
+            Assert.AreEqual(0, r.Errors.Count);
+
+            Assert.AreEqual(1, r.Data.Count);
+            var rec = r.Data[0] as HeadRecord;
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("0", rec.Source);
+        }
+
+        [Test]
+        public void CRinUnixNoEnd()
+        {
+            // No final line terminator
+            var r = BuildAndRead(linesCR, GedReader.LB.UNIX, false, false);
+            Assert.AreEqual(linesCR.Length, r.NumberLines - 1);
+            Assert.AreEqual(0, r.Errors.Count);
+
+            Assert.AreEqual(1, r.Data.Count);
+            var rec = r.Data[0] as HeadRecord;
+            Assert.IsNotNull(rec);
+            Assert.AreEqual("0", rec.Source);
+        }
+
+    }
+}
