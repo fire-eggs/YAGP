a3b1ec22743a70206bf0ec2fdb2c65e4ad5f2e39
fire-eggs <lifeattickville@gmail.com>
2017-08-14 18:20:38 -0400

    New test project: table display of all INDI records

---
+++ SharpGEDParse/IndiTable/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace IndiTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadGEDCOMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadGEDCOMToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadGEDCOMToolStripMenuItem
+            // 
+            this.loadGEDCOMToolStripMenuItem.Name = "loadGEDCOMToolStripMenuItem";
+            this.loadGEDCOMToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.loadGEDCOMToolStripMenuItem.Text = "Load GEDCOM...";
+            this.loadGEDCOMToolStripMenuItem.Click += new System.EventHandler(this.loadGEDCOMToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94.66666F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.333333F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.72464F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.275362F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(600, 345);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(561, 307);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 369);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadGEDCOMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SharpGEDParse/IndiTable/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using GEDWrap;
+using System;
+using System.Windows.Forms;
+
+// ReSharper disable LocalizableElement
+
+namespace IndiTable
+{
+    public partial class Form1 : Form
+    {
+        public event EventHandler LoadGed;
+
+        private string LastFile { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            LoadGed += Form1_LoadGed;
+        }
+
+        private void loadGEDCOMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "GEDCOM files|*.ged;*.GED";
+            ofd.FilterIndex = 1;
+            ofd.DefaultExt = "ged";
+            ofd.CheckFileExists = true;
+            if (DialogResult.OK != ofd.ShowDialog(this))
+            {
+                return;
+            }
+            LastFile = ofd.FileName; // TODO invalid ged file
+            ProcessGED(ofd.FileName);
+        }
+
+        private void ProcessGED(string gedPath)
+        {
+            Text = gedPath;
+            Application.DoEvents(); // Cycle events so image updates in case GED load/process takes a while
+            LoadGed(this, new EventArgs());
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private Forest _gedtrees;
+        //private int _visibleRows;
+
+        private List<Person> _sortedData;
+
+        private void Form1_LoadGed(object sender, EventArgs e)
+        {
+            EmptyGrid();
+
+            _gedtrees = new Forest();
+            _gedtrees.LoadGEDCOM(LastFile);
+
+            _sortedData = _gedtrees.AllPeople.ToList();
+            _sortedData.OrderBy(p => p.Id);
+
+            FillGrid();
+        }
+
+        private bool sortAscending;
+        private string sortCol;
+
+        // Set datagridview columns
+        private void LoadColumns()
+        {
+            var idCol = new DataGridViewTextBoxColumn();
+            idCol.HeaderText = "Id";
+            idCol.Name = "Id";
+            var nameCol = new DataGridViewTextBoxColumn();
+            nameCol.HeaderText = "Name";
+            nameCol.Name = "Name";
+
+            var bdateCol = new DataGridViewTextBoxColumn();
+            bdateCol.HeaderText = "Birth Date";
+            bdateCol.Name = "bdate";
+
+            dataGridView1.Columns.Add(idCol);
+            dataGridView1.Columns.Add(nameCol);
+            dataGridView1.Columns.Add(bdateCol);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.VirtualMode = true;
+            dataGridView1.CellValueNeeded += dataGridView1_CellValueNeeded;
+            dataGridView1.ColumnHeaderMouseClick += dataGridView1_ColumnHeaderMouseClick;
+
+            typeof(DataGridView).InvokeMember("DoubleBuffered",
+                BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.SetProperty,
+                null,
+                dataGridView1,
+                new object[] { true });
+
+            LoadColumns();
+        }
+
+        private void loadComplete()
+        {
+            if (dataGridView1.RowHeadersVisible) // Done it already
+                return; 
+
+            dataGridView1.RowHeadersVisible = true;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.EnableResizing;
+        }
+
+        void dataGridView1_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
+        {
+            if (_sortedData == null) return;
+
+            int dex = e.RowIndex;
+            var name = dataGridView1.Columns[e.ColumnIndex].Name;
+
+            Person p = _sortedData.ElementAt(dex);
+            switch (name)
+            {
+                case "Id":
+                    e.Value = p.Id;
+                    break;
+                case "Name":
+                    e.Value = p.Name;
+                    break;
+                case "bdate":
+                    var evt = p.GetEvent("BIRT");
+                    e.Value = evt == null ? "" : evt.Date;
+                    break;
+            }
+
+            if (dex >= dataGridView1.RowCount)
+                loadComplete();
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (sortCol.Trim().ToUpper() != dataGridView1.Columns[e.ColumnIndex].Name.Trim().ToUpper())
+            {
+                dataGridView1.Columns[sortCol].HeaderCell.SortGlyphDirection = SortOrder.None;
+                sortAscending = true;
+            }
+            else
+            {
+                sortAscending = (dataGridView1.Columns[e.ColumnIndex].HeaderCell.SortGlyphDirection != SortOrder.Ascending);
+            }
+            sortCol = dataGridView1.Columns[e.ColumnIndex].Name;
+
+            EmptyGrid(false);
+
+            // TODO *only* works if column name matches Person property name. Need comparators!!!
+            // TODO comparer for id: sort numerically, not alphabetically
+            // TODO track column, comparer, column header, etc : see GedKeeper listmanager
+
+            PropertyInfo prop = typeof(Person).GetProperty(sortCol);
+            if (prop != null)
+            {
+                if (sortAscending)
+                    _sortedData = _sortedData.OrderBy(p => prop.GetValue(p)).ToList();
+                else
+                    _sortedData = _sortedData.OrderByDescending(p => prop.GetValue(p)).ToList();
+            }
+            FillGrid();
+        }
+
+        // TODO letter navigator
+
+        private void EmptyGrid(bool reset = true)
+        {
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+
+            dataGridView1.Rows.Clear();
+
+            if (reset)
+            {
+                _gedtrees = null;
+                sortAscending = true;
+                sortCol = "Id";
+            }
+            Application.DoEvents(); // Cycle events so grid updates in case GED load/process takes a while
+        }
+
+        private void FillGrid()
+        {
+            // Populate datagridview with individuals
+            dataGridView1.RowCount = _sortedData.Count(); // TODO IEnumerable is not useful!
+            dataGridView1.Columns[sortCol].HeaderCell.SortGlyphDirection = sortAscending ? SortOrder.Ascending : SortOrder.Descending;
+        }
+    }
+}
