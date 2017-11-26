d4a986b4977b40358f036ea4a360ca40df741ba9
fire-eggs <lifeattickville@gmail.com>
2017-11-26 18:47:30 -0500

    Write more INDI data

--- SharpGEDParse/SharpGEDWriter/WriteINDI.cs
+++ SharpGEDParse/SharpGEDWriter/WriteINDI.cs
@@ -1,4 +1,5 @@
-﻿using SharpGEDParser.Model;
+﻿using System.Reflection;
+using SharpGEDParser.Model;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -22,10 +23,40 @@ namespace SharpGEDWriter
         {
             file.WriteLine("0 @{0}@ INDI", indiRecord.Ident);
 
+            WriteCommon.writeIfNotEmpty(file, "RESN", indiRecord.Restriction, 1);
+
+            // TODO original text or corrected?
+            WriteCommon.writeIfNotEmpty(file, "SEX", indiRecord.FullSex, 1);
+
+            // TODO NAME
+
+            // FAMC/FAMS
+            foreach (var indiLink in indiRecord.Links)
+            {
+                // TODO extra text
+                WriteCommon.writeXrefIfNotEmpty(file, indiLink.Tag, indiLink.Xref, 2);
+                WriteCommon.writeIfNotEmpty(file, "PEDI", indiLink.Pedi, 3);
+                WriteCommon.writeIfNotEmpty(file, "STAT", indiLink.Stat, 3);
+                WriteCommon.writeSubNotes(file, indiLink, 3);
+            }
+
             WriteEvent.writeEvents(file, indiRecord.Events, 1);
             WriteEvent.writeEvents(file, indiRecord.Attribs, 1);
 
             // TODO LDS events
+
+            foreach (var aliasLink in indiRecord.AliasLinks)
+            {
+                WriteCommon.writeXrefIfNotEmpty(file, "ALIA", aliasLink, 1);
+            }
+
+            foreach (var assoRec in indiRecord.Assocs)
+            {
+                WriteCommon.writeXrefIfNotEmpty(file, "ASSO", assoRec.Ident, 1);
+                WriteCommon.writeIfNotEmpty(file, "RELA", assoRec.Relation, 2);
+                WriteCommon.writeSubNotes(file, assoRec, 2);
+                WriteCommon.writeSourCit(file, assoRec, 2);
+            }
 
             // TODO why are INDI and FAM submitters treated different?
             foreach (var submitter in indiRecord.Submitters)
