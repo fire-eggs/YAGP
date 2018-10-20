aa50d702f8082ad9763ecf5652bb426f3ec7c7af
fire-eggs <lifeattickville@gmail.com>
2018-10-20 10:54:42 -0400

    Trying to get Travis working

---
+++ TravisCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravisCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
